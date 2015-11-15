6a503ac604870c641d5129ea4c07db62445170b6
stevenhao <stevenhao@gmail.com>
2015-11-14 23:02:53 -0500

    clean up root index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var express = require('express');
-var http = require('http')
 var path = require('path');
 
-var app = require('express')();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static('bower_components'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendfile('./index.html');
 });
 
-app.get('/tester', function(req, res){
+app.get('/tester', function(req, res) {
   res.sendfile('./tester.html');
 });
 
@@ -21,7 +20,7 @@ io.on('connection', function(socket) {
   console.log('connected to client.');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
 

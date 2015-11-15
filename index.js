21c2d99e79b8abc3e456a1aeccb0168e2917e01e
stevenhao <stevenhao@gmail.com>
2015-11-14 22:29:18 -0500

    reorganize server-side app.js and routes

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ var express = require('express');
 var http = require('http')
 var path = require('path');
 
-var app = express();
-app.set('port', process.env.PORT || 3000)
+var app = require('express')();
+var http = require('http').Server(app);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/', function(req, res){
+  res.sendfile('./index.html');
+});
 
-// app.use('/', routes);
-app.use('/', require('./routes/index'));
-
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
 module.exports = app;
